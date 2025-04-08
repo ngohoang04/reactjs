2a1d6a5d3319ca7689c00eb9d81d354ad839054f
Trong Hoang <hoangngotrong2004@gmail.com>
2025-04-09 03:45:45 +0700

    video19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,11 +6,15 @@ class MyComponent extends React.Component {
         age: 30,
         address: 'New York'
     };
+    handerClick = () => {
+        this.setState({ name: 'Jane4343 Doe' });
+    };
     render() {
         return (
             <div>
                 <h1>Hello, World!</h1>
                 {this.state.name}
+                <button onClick={this.handerClick} >click me</button>
             </div>
         );
     }
