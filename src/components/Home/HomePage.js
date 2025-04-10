258168090a0aef1f5b51cfe1186d3a8949cf0b74
Trong Hoang <hoangngotrong2004@gmail.com>
2025-04-10 19:37:57 +0700

    video54

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -6,6 +6,11 @@ const HomePage = () => {
             <video autoPlay loop muted >
                 <source src={videoHomePage} type="video/mp4" />
             </video>
+            <div claassName="homepage-content">
+                <h1 claassName="title-1">Welcome to Our Website</h1>
+                <p claassName="title-2">Your one-stop solution for all your needs.</p>
+                <button claassName="title-3">get started</button>
+            </div>
         </div>
     );
 }
